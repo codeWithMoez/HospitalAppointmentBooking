f328345b2d58afb6d3757ad68b75cbc6b1e8d7ff
codeWithMoez <221073@students.au.edu.pk>
2025-06-17 22:56:31 +0500

    Added AppointmentDetail Data transfer object

---
+++ HospitalAppointmentBooking.Application/DTOs/AppointmentDetailDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAppointmentBooking.Application.DTOs
+{
+    public class AppointmentDetailDto
+    {
+        public int AppointmentId { get; set; }
+        public DateTime Date { get; set; }
+        public string? Remarks { get; set; }
+
+        public int DoctorId { get; set; }
+        public string? DoctorName { get; set; } 
+        public string? Specialization { get; set; } 
+
+        public int PatientId { get; set; }
+        public string? PatientName { get; set; } 
+        public string? Email { get; set; } 
+    }
+
+}
