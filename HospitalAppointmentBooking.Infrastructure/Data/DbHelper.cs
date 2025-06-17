85b3487c59b5e107dc40ec22a611de5aa7c3918b
codeWithMoez <221073@students.au.edu.pk>
2025-06-17 23:30:09 +0500

    Completed the DbHelper Class

---
+++ HospitalAppointmentBooking.Infrastructure/Data/DbHelper.cs
@@ -0,0 +1,21 @@
+﻿using HospitalAppointmentBooking.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace HospitalAppointmentBooking.Infrastructure.Data
+{
+    public class DbHelper : IDbHelper
+    {
+        private readonly string _connectionString;
+
+        public DbHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("HospitalAppointmentBooking");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
