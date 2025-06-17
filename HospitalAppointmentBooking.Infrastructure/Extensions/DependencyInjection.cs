52a73c1d49fd469478c44cae23517651ebb9010f
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 01:25:00 +0500

    Changes made added AppointmentQueryService

---
+++ HospitalAppointmentBooking.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using HospitalAppointmentBooking.Application.Interfaces;
+using HospitalAppointmentBooking.Domain.Entities;
+using HospitalAppointmentBooking.Infrastructure.Data;
+using HospitalAppointmentBooking.Infrastructure.Repository;
+using HospitalAppointmentBooking.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalAppointmentBooking.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            // DB
+            services.AddScoped<IDbHelper, DbHelper>(); 
+
+            // Repository
+            services.AddScoped<IRepository<Doctor>, DoctorRepository>();
+            services.AddScoped<IRepository<Patient>, PatientRepository>();
+            services.AddScoped<IRepository<Appointment>, AppointmentRepository>();
+
+            // Services
+            services.AddScoped<IAppointmentQueryService, AppointmentQueryService>();
+
+            return services;
+        }
+    }
+}
