6a680b355e97573aa539ccca28007e5714c0ccbb
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 12:16:03 +0500

    Dependency Extensions

---
+++ HospitalAppointmentBooking.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿
+using HospitalAppointmentBooking.Application.Interfaces;
+using HospitalAppointmentBooking.Application.Services;
+using HospitalAppointmentBooking.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HospitalAppointmentBooking.Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IServices<Doctor>, DoctorService>();
+            services.AddScoped<IServices<Patient>, PatientService>();
+            services.AddScoped<IServices<Appointment>, AppointmentService>();
+
+            return services;
+        }
+    }
+}
