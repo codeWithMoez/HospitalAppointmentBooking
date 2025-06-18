48d538cd7bf71eeee4fe13639fe69022c420ce17
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 21:05:36 +0500

    Added [Required]

--- HospitalAppointmentBooking.Domain/Entities/Doctor.cs
+++ HospitalAppointmentBooking.Domain/Entities/Doctor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace HospitalAppointmentBooking.Domain.Entities
     public class Doctor
     {
         public int DoctorId { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Specialization { get; set; }
     }
 }
