6230da1e3539e0d70f3facce0f6dd92ec010d075
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 21:07:14 +0500

    Added Pages

--- HospitalAppointmentBooking.BlazorUI/Program.cs
+++ HospitalAppointmentBooking.BlazorUI/Program.cs
@@ -1,10 +1,21 @@
+using HospitalAppointmentBooking.Application.Extensions;
 using HospitalAppointmentBooking.BlazorUI.Components;
+using HospitalAppointmentBooking.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddInfrastructure(builder.Configuration).AddApplication();
+var apiBase = builder.Configuration["ApiBaseUrl"];
+builder.Services.AddHttpClient("HospitalApi", client =>
+{
+    client.BaseAddress = new Uri(apiBase!);
+});
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("HospitalApi"));
+
 
 var app = builder.Build();
 
