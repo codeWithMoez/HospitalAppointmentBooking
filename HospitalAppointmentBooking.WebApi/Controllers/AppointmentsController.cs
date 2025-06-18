dfe0a5ad8eeb74065842c388f1220d723a27661d
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 12:43:48 +0500

    Added Controllers Api

---
+++ HospitalAppointmentBooking.WebApi/Controllers/AppointmentsController.cs
@@ -0,0 +1,28 @@
+﻿using HospitalAppointmentBooking.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAppointmentBooking.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IAppointmentQueryService _queryService;
+        public AppointmentsController(IAppointmentQueryService queryService)
+        {
+            _queryService = queryService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_queryService.GetAllDetails());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_queryService.GetDetailById(id));
+        }
+    }
+}
