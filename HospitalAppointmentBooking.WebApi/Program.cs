c392af742c4a2589fc3f736ee06a7c5313223ac5
codeWithMoez <221073@students.au.edu.pk>
2025-06-18 12:48:08 +0500

    Added Service in Program.cs

--- HospitalAppointmentBooking.WebApi/Program.cs
+++ HospitalAppointmentBooking.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using HospitalAppointmentBooking.Application.Extensions;
+using HospitalAppointmentBooking.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddInfrastructure(builder.Configuration)
+    .AddApplication();
 
 var app = builder.Build();
 
